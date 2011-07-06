a4a3a200aa9edbe6005c6518a72f513783f83948
Enrico Rizzardi <enrico.rizzardi@gmail.com>
2011-07-06 15:00:44 +0200

    Aggiunti bottoni, albero e properties a destra

--- wefr/src/PSI/MainWindow.java
+++ wefr/src/PSI/MainWindow.java
@@ -13,6 +13,17 @@ import javax.swing.KeyStroke;
 import java.awt.event.KeyEvent;
 import java.awt.event.InputEvent;
 import javax.swing.JSeparator;
+import javax.swing.JTree;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import javax.swing.JInternalFrame;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Component;
+import javax.swing.Box;
+import java.awt.FlowLayout;
 
 public class MainWindow extends JFrame {
 
@@ -40,7 +51,7 @@ public class MainWindow extends JFrame {
 	public MainWindow() {
 		setTitle("EUD-MAMBA");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 564, 386);
 		
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
@@ -59,9 +70,6 @@ public class MainWindow extends JFrame {
 		mntmSave.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_MASK));
 		mnFile.add(mntmSave);
 		
-		JMenuItem mntmSaveAs = new JMenuItem("Save as...");
-		mnFile.add(mntmSaveAs);
-		
 		JSeparator separator = new JSeparator();
 		mnFile.add(separator);
 		
@@ -73,22 +81,31 @@ public class MainWindow extends JFrame {
 		menuBar.add(mnEdit);
 		
 		JMenuItem mntmUndo = new JMenuItem("Undo");
+		mntmUndo.setEnabled(false);
 		mnEdit.add(mntmUndo);
 		
 		JMenuItem mntmRedo = new JMenuItem("Redo");
 		mnEdit.add(mntmRedo);
+		mntmRedo.setEnabled(false);
+
 		
 		JSeparator separator_1 = new JSeparator();
 		mnEdit.add(separator_1);
 		
 		JMenuItem mntmCut = new JMenuItem("Cut");
 		mnEdit.add(mntmCut);
+		mntmCut.setEnabled(false);
+
 		
 		JMenuItem mntmCopy = new JMenuItem("Copy");
 		mnEdit.add(mntmCopy);
+		mntmCopy.setEnabled(false);
+
 		
 		JMenuItem mntmPaste = new JMenuItem("Paste");
 		mnEdit.add(mntmPaste);
+		mntmPaste.setEnabled(false);
+
 		
 		JMenu mnOptions = new JMenu("TODO Options");
 		menuBar.add(mnOptions);
@@ -97,7 +114,37 @@ public class MainWindow extends JFrame {
 		mnOptions.add(mntmOptions);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JTree tree = new JTree();
+		tree.setBounds(5, 49, 216, 281);
+		contentPane.add(tree);
+		
+		JInternalFrame internalFrame = new JInternalFrame("Properties");
+		internalFrame.setBounds(230, 49, 320, 274);
+		contentPane.add(internalFrame);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panel.setBounds(5, 0, 175, 37);
+		contentPane.add(panel);
+		
+		JButton button = new JButton("");
+		button.setToolTipText("Open");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		button.setIcon(new ImageIcon(MainWindow.class.getResource("/com/sun/java/swing/plaf/motif/icons/TreeOpen.gif")));
+		panel.add(button);
+		
+		JButton button_1 = new JButton("");
+		panel.add(button_1);
+		
+		JButton button_2 = new JButton("");
+		panel.add(button_2);
+		internalFrame.setVisible(true);
 	}
 }
