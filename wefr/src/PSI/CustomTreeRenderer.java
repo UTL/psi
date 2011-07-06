c9f7148dcb0eedfd83bbbe5ab905f890a036ce19
Enrico Rizzardi <enrico.rizzardi@gmail.com>
2011-07-06 16:29:57 +0200

    Modificato il renderer, ma non va un corno (probabilmente problema di permessi, o di path dell'icona)

--- wefr/src/PSI/CustomTreeRenderer.java
+++ wefr/src/PSI/CustomTreeRenderer.java
@@ -1,6 +1,7 @@
 package PSI;
 
 import java.awt.Component;
+import java.io.File;
 
 import javax.swing.ImageIcon;
 import javax.swing.tree.DefaultMutableTreeNode;
@@ -17,14 +18,21 @@ public class CustomTreeRenderer extends DefaultTreeCellRenderer {
 	
 	public CustomTreeRenderer() {
 		super();
+		
+		File schema = new File("/home/enrico/Immagini/Flash_icon.gif");	
+
+		if( schema.canRead())
+			System.out.println("LEGGIBILE");
+		else
+			System.out.println("NON LEGGIBILE");
+		
 		//TODO cercare delle icone per l'albero JTree e metterle nel path corretto
-		rootIcon = new ImageIcon(CustomTreeRenderer.class.getResource("/images/defaultIcon.gif"));
-		linkIcon = new ImageIcon(CustomTreeRenderer.class.getResource("/images/specialIcon.gif"));
+		rootIcon = new ImageIcon(CustomTreeRenderer.class.getResource("test_icon.gif"));
+		/*linkIcon = new ImageIcon(CustomTreeRenderer.class.getResource("/images/specialIcon.gif"));
 		alternativeIcon = new ImageIcon(CustomTreeRenderer.class.getResource("/images/defaultIcon.gif"));
 		compositeIcon = new ImageIcon(CustomTreeRenderer.class.getResource("/images/specialIcon.gif"));
 		textIcon = new ImageIcon(CustomTreeRenderer.class.getResource("/images/defaultIcon.gif"));
-		imageIcon = new ImageIcon(CustomTreeRenderer.class.getResource("/images/specialIcon.gif"));
-		init();
+//		imageIcon = new ImageIcon(CustomTreeRenderer.class.getResource("/images/specialIcon.gif"));*/
 	}
 	
 	public Component getTreeCellRendererComponent(Albero tree,
@@ -35,22 +43,15 @@ public class CustomTreeRenderer extends DefaultTreeCellRenderer {
 		          expanded, leaf, row, hasFocus);
 
 		        Object nodeObj = ((DefaultMutableTreeNode)value).getUserObject();
+		        setIcon(rootIcon);
 		        // check whatever you need to on the node user object
-		        if (true) { //TODO verificare il tipo dell'oggetto // ((WhateverNodeObjectClass)nodeObj).someProperty
+		        /*if (true) { //TODO verificare il tipo dell'oggetto // ((WhateverNodeObjectClass)nodeObj).someProperty
 		            setIcon(rootIcon);
 		        } else {
 		            setIcon(linkIcon);
-		        } 
+		        } */
 		        return this;
 		    }
 	
-	private void init(){
-		    ImageIcon defaultIcon;
-		    ImageIcon specialIcon;
-		    //TODO aggiungere icon
-		    
-
-		    
-		}
 	
 }
