5e56cc9ead4b0a281216a01c9c3cfd4dd6d1786e
Enrico Rizzardi <enrico.rizzardi@gmail.com>
2011-07-27 12:23:33 +0200

    Ora gli eventi contengono anche un oggetto di tipo componente (utile per il passaggio dell'oggetto da un JFrame all'altro)

--- wefr/src/webApplication/grafica/MyEventClass.java
+++ wefr/src/webApplication/grafica/MyEventClass.java
@@ -1,13 +1,26 @@
 package webApplication.grafica;
 
+import webApplication.business.Componente;
+
 public class MyEventClass extends java.util.EventObject {
     /**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private Componente componente;
 
 	//here's the constructor
     public MyEventClass(Object source) {
         super(source);
+    }
+    
+    public MyEventClass(Object source, Componente comp) {
+        super(source);
+        this.componente = comp;
+    }
+    
+    public Componente getComponente(){
+    	//TODO sarebbe bello usare il metodo clone di andrea
+    	return componente;
     }
 }
--- wefr/src/webApplication/grafica/MyEventClassListener.java
+++ wefr/src/webApplication/grafica/MyEventClassListener.java
@@ -4,4 +4,6 @@ import java.util.EventObject;
 
 public interface MyEventClassListener {
 	public abstract void handleMyEventClassEvent(EventObject e);
+
+	void handleMyEventClassEvent(MyEventClass e);
 }
