f0568e9939d16b9c94c1660617f8f58a6c77c800
Enrico Rizzardi <enrico.rizzardi@gmail.com>
2011-08-01 10:23:30 +0200

    Aggiornato componente

--- wefr/src/webApplication/business/Componente.java
+++ wefr/src/webApplication/business/Componente.java
@@ -8,120 +8,144 @@ package webApplication.business;
 import java.io.*;
 
 /**
- *
+ * 
  * @author Landi Jessica & Miglioranzi Marco
  */
 
+public abstract class Componente implements Serializable, Cloneable {
 
-public abstract class Componente implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7375591465729638889L;
+	/*
+	 * visibilita' 0 = necessary1 = Indifferent2 = expendable
+	 * 
+	 * enfasi 0 = Greatly"); 1 = Normally"); 2 = Not at all
+	 */
+	private String nome;
+	private String categoria;
+	private int visibilita;
+	private int enfasi;
+	private String type;
 
-	 /*
-	  * visibilita'
-	  * 0 = necessary
-	  *1 = Indifferent
-	  *2 = expendable
-   
-     enfasi
-     	0 = Greatly");
-        1 = Normally");
-        2 = Not at all
-        */
-    private String nome;
-    private String categoria;
-    private int visibilita;
-    private int enfasi;
+	/**
+	 * Costruzione di un componente
+	 */
+	public Componente(String n, String c, int v, int e, String tp) {
 
-    /**
-     * Costruzione di un componente
-     */
-    public Componente(String n,String c,int v,int e){
+		nome = n;
+		categoria = c;
+		visibilita = v;
+		enfasi = e;
+		type = tp;
 
-        nome=n;
-        categoria=c;
-        visibilita=v;
-        enfasi=e;
+	}
 
-    }
+	/**
+	 * Metodo che permette la modifica del nome del componente
+	 * 
+	 * @param n
+	 */
+	public void setNome(String n) {
 
- 
-    /**
-     * Metodo che permette la modifica del nome del componente
-     * @param n
-     */
-    public void setNome(String n){
+		nome = n;
 
-        nome=n;
-    
-    }
+	}
 
-    /**
-     *
-     * @return nome del componente
-     */
-    public String getNome(){
+	/**
+	 * 
+	 * @return nome del componente
+	 */
+	public String getNome() {
 
-        return nome;
+		return nome;
 
-    }
+	}
 
-    /**
-    * Metodo per il settaggio della categoria del componente
-    * @param c
-    */
-    public void setCategoria(String c){
+	/**
+	 * Metodo per il settaggio della categoria del componente
+	 * 
+	 * @param c
+	 */
+	public void setCategoria(String c) {
 
-        categoria=c;
+		categoria = c;
 
-    }
+	}
 
-    /**
-     *
-     * @return categoria del componente
-     */
-    public String getCategoria(){
+	/**
+	 * 
+	 * @return categoria del componente
+	 */
+	public String getCategoria() {
 
-        return categoria;
+		return categoria;
 
-    }
+	}
 
-    /**
-     * Metodo che modifica la visibilità del componente
-     * @param v
-     */
-    public void setVisibilita(int v){
+	/**
+	 * Metodo che modifica la visibilità del componente
+	 * 
+	 * @param v
+	 */
+	public void setVisibilita(int v) {
 
-        visibilita=v;
-    
-    }
+		visibilita = v;
 
-    /**
-     *
-     * @return la visbilità del componente
-     */
-    public int getVisibilita(){
+	}
 
-        return visibilita;
+	/**
+	 * 
+	 * @return la visbilità del componente
+	 */
+	public int getVisibilita() {
 
-    }
+		return visibilita;
 
-    /**
-     * Metodo che modifica l'anfasi del componente
-     * @param e
-     */
-    public void setEnfasi(int e){
+	}
 
-        enfasi=e;
-    
-    }
+	/**
+	 * Metodo che modifica l'anfasi del componente
+	 * 
+	 * @param e
+	 */
+	public void setEnfasi(int e) {
 
-    /**
-     *
-     * @return l'enfasi del componente
-     */
-    public int getEnfasi(){
-        
-        return enfasi;
+		enfasi = e;
 
-    }
+	}
 
+	/**
+	 * 
+	 * @return l'enfasi del componente
+	 */
+	public int getEnfasi() {
+
+		return enfasi;
+
+	}
+	
+	public String toString()	{
+		return nome;
+	}
+	
+	public String getType()	{
+		return type;
+	}
+	
+	public void setType(String t)	{
+		type = t;
+	}
+	
+	public Componente clone()	{
+		try {
+			return (Componente) super.clone();
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+		return null;
+		
+	}
+	
 }
